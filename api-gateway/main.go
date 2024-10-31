559a50f3d74635fbf96215c64453bb40336727fe
Yannik Maaßen <yannik.maassen@t-online.de>
2024-10-31 12:25:33 +0530

    fix redirect issues

--- api-gateway/main.go
+++ api-gateway/main.go
@@ -11,17 +11,17 @@ func main() {
 
 	router.GET("/users/*action", func(c *gin.Context) {
 		action := c.Param("action")
-		c.Redirect(http.StatusTemporaryRedirect, "http://user-service:8081/users"+action)
+		c.Redirect(http.StatusTemporaryRedirect, "http://localhost:8081/users"+action)
 	})
 
 	router.GET("/products/*action", func(c *gin.Context) {
 		action := c.Param("action")
-		c.Redirect(http.StatusTemporaryRedirect, "http://product-service:8082/products"+action)
+		c.Redirect(http.StatusTemporaryRedirect, "http://localhost:8082/products"+action)
 	})
 
 	router.GET("/orders/*action", func(c *gin.Context) {
 		action := c.Param("action")
-		c.Redirect(http.StatusTemporaryRedirect, "http://order-service:8083/orders"+action)
+		c.Redirect(http.StatusTemporaryRedirect, "http://localhost:8083/orders"+action)
 	})
 
 	router.Run(":8080")
